2a7299a844f4d24f2dfaf2e8046f0f5b1b375515
AnastasiaGer <aagermanova@gmail.com>
2021-07-23 20:10:07 -0700

    Provider

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,4 +1,6 @@
 function posts (state = [], action) {
+  console.log('Changed');
+  
   console.log(state, action);
   return state;
 }
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 // Import css
 import css from './styles/style.styl';
@@ -14,12 +16,14 @@ import PhotoGrid from './components/PhotoGrid';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
-)
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
+);
 
 render(router, document.getElementById('root'));
